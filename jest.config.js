d12e7ef2eea20f78964bcb5202f806749824e061
Carlos <carlosfelixpenha@gmail.com>
2026-01-16 17:56:47 -0300

    commit + push = CI rodando sozinho.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   testEnvironment: "jsdom",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
 };
